f7b84c37a7cfa50391042f73c7f6c1019436c70d
Zacky B Viriot <89651654+ZackyViriot@users.noreply.github.com>
2024-11-21 23:18:52 -0600

    trying somethign

--- src/pages/signUpAndLogin/LoginForm.tsx
+++ src/pages/signUpAndLogin/LoginForm.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../axios/axiosSetup";
+import axios from "axios";
 
 export default function LoginForm() {
   const [formData, setFormData] = useState({
@@ -21,7 +22,7 @@ export default function LoginForm() {
     e.preventDefault();
     // Add your login logic here when we add backend 
     try {
-      const res = await axiosInstance.post('/auth/login',formData);
+      const res = await axios.post('https://studybuddy-production-1dcc.up.railway.app/auth/login',formData);
       const token = res.data.token;
       // need to set to local storage to be able to use it throught the application.
       localStorage.setItem('token',token)
